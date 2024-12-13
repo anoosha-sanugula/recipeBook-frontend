372dfb680869a4ba3492e73af409eb516a4fe943
anoosha-sanugula <anoosha.sanugula@everest.engineering>
2024-12-13 13:02:12 +0530

    Add router tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,46 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import App from "../App";
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from '../App'
 
-test("renders learn react link", () => {
-  render(<App />);
-  const welcomeMessage = screen.getByText(/Welcome to RecipeBook/i);
-  expect(welcomeMessage).toBeInTheDocument();
+describe("App Component", () => {
+  test('renders Welcomepage on "/" route', () => {
+    render(
+      <MemoryRouter
+        initialEntries={["/"]}
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
+        <App />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByText(/Recipes Right for Your Family/i)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Start Cooking!/i)).toBeInTheDocument();
+  });
+
+  test('renders Register page on "/register" route', () => {
+    render(
+      <MemoryRouter
+        initialEntries={["/register"]}
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/hello/i)).toBeInTheDocument();
+  });
+
+  test('navigates to Register page when "Start Cooking!" button is clicked', () => {
+    render(
+      <MemoryRouter
+        initialEntries={["/"]}
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
+        <App />
+      </MemoryRouter>
+    );
+    fireEvent.click(screen.getByText(/Start Cooking!/i));
+    expect(screen.getByText(/hello/i)).toBeInTheDocument();
+  });
 });
