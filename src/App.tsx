b050b3283ff15c069954f9975799cfb7a4a49486
anoosha-sanugula <anoosha.sanugula@everest.engineering>
2024-12-16 13:51:39 +0530

    Add async storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../src/styles/App.css";
 import { Routes, Route } from "react-router-dom";
 import Welcomepage from "./components/welcome/WelcomePage";
 import Register from "./components/register/Register";
 import HomePage from "./components/home/HomePage";
 import Login from "./components/login/Login";
-import { UserProvider } from "./components/context/UserContext";
 
 function App() {
+  const [isLogin, setIsLogin] = useState<boolean | null>(null);
+  useEffect(() => {
+    const loadUserData = async () => {
+      try {
+        const userDataString = localStorage.getItem("userdata");
+        if (userDataString) {
+          setIsLogin(true);
+        } else {
+          setIsLogin(false);
+        }
+      } catch (error) {
+        setIsLogin(false);
+      }
+    };
+    loadUserData();
+  }, []);
+
   return (
     <div className="App">
-      <UserProvider>
-        <Routes>
+      <Routes>
+        {isLogin ? (
+          <Route path="/" element={<HomePage />} />
+        ) : (
           <Route path="/" element={<Welcomepage />} />
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="home" element={<HomePage />} />
-        </Routes>
-      </UserProvider>
+        )}
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="home" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
