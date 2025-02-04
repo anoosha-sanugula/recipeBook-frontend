5e6087d5ee772362129375f605a34f440d9867e7
anoosha-sanugula <anoosha.sanugula@everest.engineering>
2025-02-04 16:31:07 +0530

    Modify the code

--- src/pages/recipe/RecipeCard.test.tsx
+++ src/pages/recipe/RecipeCard.test.tsx
@@ -43,9 +43,26 @@ describe("RecipeCard Component", () => {
       ok: true,
       json: jest.fn().mockResolvedValue(mockRecipe),
     });
+    localStorage.clear();
+  });
+  test("renders UnAuthenticatedPage when not authenticated", async () => {
+    render(
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <RecipeCard />
+      </Router>
+    );
+
+    await waitFor(() => {
+      expect(
+        screen.getByText("Please log in or register to continue.")
+      ).toBeInTheDocument();
+      expect(screen.getByText("Login")).toBeInTheDocument();
+      expect(screen.getByText("Register")).toBeInTheDocument();
+    });
   });
 
-  test("renders recipe image, title, and description correctly", async () => {
+  test("renders recipe image, title, and description correctly when authenticated", async () => {
+    localStorage.setItem("accessToken", "mockedAccessToken");
     render(
       <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <RecipeCard />
@@ -54,17 +71,14 @@ describe("RecipeCard Component", () => {
 
     await waitFor(() => {
       expect(screen.getByAltText("Tomato")).toBeInTheDocument();
-      expect(screen.getByAltText("Tomato")).toHaveAttribute(
-        "src",
-        mockRecipe.imageUrl
-      );
-
       expect(screen.getByText("Tomato")).toBeInTheDocument();
       expect(screen.getByText(mockRecipe.description)).toBeInTheDocument();
     });
   });
 
   test("renders rating correctly", async () => {
+    localStorage.setItem("accessToken", "mockedAccessToken");
+
     render(
       <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <RecipeCard />
@@ -75,11 +89,13 @@ describe("RecipeCard Component", () => {
       expect(
         screen.getByText(`Rating: ${mockRecipe.rating}`)
       ).toBeInTheDocument();
-      expect(screen.getByAltText("rating")).toBeInTheDocument(); 
+      expect(screen.getByAltText("rating")).toBeInTheDocument();
     });
   });
 
   test("renders ingredients correctly", async () => {
+    localStorage.setItem("accessToken", "mockedAccessToken");
+
     render(
       <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <RecipeCard />
@@ -110,6 +126,20 @@ describe("RecipeCard Component", () => {
       expect(
         screen.getByAltText(mockRecipe.Ingredients[1].name)
       ).toHaveAttribute("src", mockRecipe.Ingredients[1].imageUrl);
+    });
+  });
+  test("handles error in fetch", async () => {
+    localStorage.setItem("accessToken", "mockedAccessToken");
+    global.fetch = jest.fn().mockRejectedValueOnce(new Error("Network Error"));
+
+    render(
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <RecipeCard />
+      </Router>
+    );
+
+    await waitFor(() => {
+      expect(global.alert).toHaveBeenCalledWith("Error fetching recipe");
     });
   });
 });
--- src/pages/recipe/RecipeCard.tsx
+++ src/pages/recipe/RecipeCard.tsx
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Recipe } from "./Recipe.type";
 import "./RecipeCard.css";
+import UnAuthenticatedPage from "../unauthenticated/UnAuthenticatedPage";
 const RecipeCard = () => {
   const { recipeId } = useParams<{ recipeId: string }>();
   const [recipe, setRecipe] = useState<Recipe | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, [navigate]);
   const extractVideoId = (url: string) => {
     const regex =
       /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^/\n\s]+\/\S+\/|\S+\?v=)|youtu\.be\/)([\w-]+)(?:[?&]t=[\d\w-]+)?/;
@@ -20,8 +32,19 @@ const RecipeCard = () => {
   useEffect(() => {
     const fetchRecipe = async () => {
       const url = `http://localhost:3000/recipebook/recipes/${recipeId}`;
+      const token = localStorage.getItem("accessToken");
+
+      const headers: HeadersInit = token
+        ? { Authorization: `Bearer ${token}` }
+        : {};
       try {
-        const response = await fetch(url, { method: "GET" });
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            ...headers,
+            "Content-Type": "application/json",
+          },
+        });
         if (response.ok) {
           const recipeData = await response.json();
           setRecipe(recipeData);
@@ -39,62 +62,72 @@ const RecipeCard = () => {
   }
 
   return (
-    <div className="recipe-page">
-      <img
-        src={recipe.imageUrl || "/assets/recdef.jpg"}
-        alt={recipe.title}
-        className="recipe-img"
-      />
-      <div className="recipe-details">
-        <h1>{recipe.title}</h1>
-        <p>{recipe.description}</p>
-        <div className="recipe-rating">
-          <p>Rating: {recipe.rating}</p>
-          <img src="/assets/rating.png" alt="rating" className="rating-image" />
-        </div>
-        <h3>Instructions</h3>
-        <p>{recipe.instructions}</p>
-        <h3>Ingredients</h3>
-        <div className="ingredient-list">
-          {recipe.Ingredients && recipe.Ingredients.length > 0 ? (
-            recipe.Ingredients.map((ingredient, index) => (
-              <div className="ingredient-item" key={index}>
-                {ingredient.imageUrl && (
-                  <img
-                    src={ingredient.imageUrl}
-                    alt={ingredient.name}
-                    className="ingredient-image"
-                  />
-                )}
-                <div className="ingredient-info">
-                  <span className="ingredient-quantity">
-                    {ingredient.quantity} {ingredient.unit}
-                  </span>
-                  <span className="ingredient-name">{ingredient.name}</span>
-                </div>
-              </div>
-            ))
-          ) : (
-            <p>No ingredients listed.</p>
-          )}
-        </div>
-        <div>
-          {embedUrl ? (
-            <div className="video-container">
-              <iframe
-                width="560"
-                height="315"
-                src={embedUrl}
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
+    <div>
+      {isAuthenticated ? (
+        <div className="recipe-page">
+          <img
+            src={recipe.imageUrl || "/assets/recdef.jpg"}
+            alt={recipe.title}
+            className="recipe-img"
+          />
+          <div className="recipe-details">
+            <h1>{recipe.title}</h1>
+            <p>{recipe.description}</p>
+            <div className="recipe-rating">
+              <p>Rating: {recipe.rating}</p>
+              <img
+                src="/assets/rating.png"
+                alt="rating"
+                className="rating-image"
+              />
             </div>
-          ) : (
-            <p>No video available.</p>
-          )}
+            <h3>Instructions</h3>
+            <p>{recipe.instructions}</p>
+            <h3>Ingredients</h3>
+            <div className="ingredient-list">
+              {recipe.Ingredients && recipe.Ingredients.length > 0 ? (
+                recipe.Ingredients.map((ingredient, index) => (
+                  <div className="ingredient-item" key={index}>
+                    {ingredient.imageUrl && (
+                      <img
+                        src={ingredient.imageUrl}
+                        alt={ingredient.name}
+                        className="ingredient-image"
+                      />
+                    )}
+                    <div className="ingredient-info">
+                      <span className="ingredient-quantity">
+                        {ingredient.quantity} {ingredient.unit}
+                      </span>
+                      <span className="ingredient-name">{ingredient.name}</span>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p>No ingredients listed.</p>
+              )}
+            </div>
+            <div>
+              {embedUrl ? (
+                <div className="video-container">
+                  <iframe
+                    width="560"
+                    height="315"
+                    src={embedUrl}
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              ) : (
+                <p>No video available.</p>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <UnAuthenticatedPage />
+      )}
     </div>
   );
 };
